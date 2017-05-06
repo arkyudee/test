8d6770e6fb122e0c8c318f84ad69a940df94d11a
jonelmawirat <jonel.mawirat@bugomi.com>
2017-05-06 04:20:23 +0000

    Activity

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-#this is a comment
+import os
 
-#wakoko
-#3rd
-#4th
-#5th
+response1 = os.system("ping -c 1 8.8.8.8")
+response2 = os.system("ping -c 1 8.8.8.4")
+response3 = os.system("ping -c 1 192.168.1.1")
+response4 = os.system("ping -c 1 192.168.1.2")
 
+
+if response1 == 256:
+    print("8.8.8.8: Unreacheable")
+else:
+    print("8.8.8.8: Reacheable")
+if response2 == 256:
+    print("8.8.8.4: Unreacheable")
+else:
+    print("8.8.8.4: Reacheable")
+if response3 == 256:
+    print("192.168.1.1: Unreacheable")
+else:
+    print("192.168.1.1: Reacheable")
+if response4 == 256:
+    print("192.168.1.2: Unreacheable")
+else:
+    print("192.168.1.2: Reacheable")
