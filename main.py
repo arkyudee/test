c8f9b685e95b5b37075d22da303803b115738a79
Yatten <jonel.mawirat@bugomi.com>
2017-05-06 08:12:38 +0000

    conft

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
-import os
-
-response1 = os.system("ping -c 1 8.8.8.8")
-response2 = os.system("ping -c 1 8.8.8.4")
-response3 = os.system("ping -c 1 192.168.1.1")
-response4 = os.system("ping -c 1 192.168.1.2")
-
-
-if response1 == 256:
-    print("8.8.8.8: Unreacheable")
-else:
-    print("8.8.8.8: Reacheable")
-if response2 == 256:
-    print("8.8.8.4: Unreacheable")
-else:
-    print("8.8.8.4: Reacheable")
-if response3 == 256:
-    print("192.168.1.1: Unreacheable")
-else:
-    print("192.168.1.1: Reacheable")
-if response4 == 256:
-    print("192.168.1.2: Unreacheable")
-else:
-    print("192.168.1.2: Reacheable")
+import pexpect
+child = pexpect.spawn("telnet 54.250.238.180 32773")
+child.sendline("\r\n")
+child.expect(">")
+child.sendline("enable")
+child.expect("#")
+child.sendline("config terminal")
+child.expect("#")
+child.sendline("hostname Router1")
+child.expect("#")
+child.sendline("end")
+child.expect("#")
